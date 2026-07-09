544cdd42b3f1ede76f9b2f19cce31f9e618f997e
Aathee1103 <80621864+Aathee1103@users.noreply.github.com>
2026-07-09 14:57:22 +0200

    Add files via upload

--- src/my_vision_pkg/launch/aud_ros.launch.py
+++ src/my_vision_pkg/launch/aud_ros.launch.py
@@ -3,28 +3,45 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    image_node = Node(
-        package='my_vision_pkg',
-        executable='image_emotion',
-        name='image_emotion_node',
-        output='screen'
-    )
+    #image_node = Node(
+    #    package='my_vision_pkg',
+    #    executable='image_emotion',
+    #    name='image_subscriber'',
+    #    output='screen'
+    #)
 
     audio_node = Node(
         package='my_vision_pkg',
         executable='audio_rec',
-        name='audio_recorder',
+        name='voice_to_speech',
         output='screen'
     )
     moveit_node = Node(
         package='my_vision_pkg',
-        executable='moveit_node',
-        name='list_moveit_groups',
+        executable='moveit_arm',
+        name='moveit_arm_cartesian',
+        output='screen'
+    )
+    marker_node = Node(
+        package='my_vision_pkg',
+        executable='marker_align',
+        name='aruco_docking',
+        output='screen'
+    )
+    table_node = Node(
+        package='my_vision_pkg',
+        executable='table_detect',
+        name='table_detector',
         output='screen'
     )
 
+
+    #return LaunchDescription([
+    #    image_node,
+    #    audio_node,
+    #    moveit_node
+    #])
     return LaunchDescription([
-        image_node,
         audio_node,
         moveit_node
     ])
